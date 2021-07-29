2a6274021395454115b83e681320023279470351
tawanacode <info@tawanacode.com>
2021-07-29 23:11:12 +0200

    Setup search service and get weather data from api

---
+++ src/app/models/api.enum.ts
@@ -0,0 +1,4 @@
+export enum Api {
+    KEY = 'xxxx',
+    URL = 'https://api.openweathermap.org/data/2.5',
+}
---
+++ src/app/services/search.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map, shareReplay, tap } from 'rxjs/operators';
+import { WeatherResponse, DailyWeather, CoodinatesResponse, Coodinates, Api } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  private readonly API_key: string = Api.KEY;
+  private readonly WEATHER_API: string = Api.URL;
+
+  constructor(private http: HttpClient) { }
+
+  public searchByCity(city: string): Observable<Coodinates> {
+
+    const searchParameters = new HttpParams().set('q', city).set('appid', this.API_key);
+
+    return this.http.get<CoodinatesResponse>(`${this.WEATHER_API}/weather`, { params: searchParameters }).pipe(
+      map(res => res?.coord), // Recommended replacement for pluck as per rxjs docs since pluck is soon to be depreciated
+      catchError(this.handleError),
+      tap(console.log),
+      shareReplay()
+    );
+  }
+
+  public searchByCoodinates(coords: Coodinates): Observable<DailyWeather> {
+
+    const searchParameters = new HttpParams().set('lon', coords.lon).set('lat', coords.lat).set('exclude', 'current, minutely, hourly, alerts').set('appid', this.API_key);
+
+    return this.http.get<WeatherResponse>(`${this.WEATHER_API}/onecall`, { params: searchParameters }).pipe(
+      map(res => res?.daily.slice(0, 5).map((data: DailyWeather) => this.getFilteredData(data))),
+      catchError(this.handleError),
+      tap(console.log),
+      shareReplay()
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+    }
+
+    console.error(errorMessage);
+    // TODO print this error to the UI
+    return throwError(errorMessage);
+  }
+
+  private getFilteredData(weatherData: DailyWeather): Partial<DailyWeather> {
+    const { dt, temp, weather } = weatherData;
+    return { dt, temp, weather };
+  }
+
+}
