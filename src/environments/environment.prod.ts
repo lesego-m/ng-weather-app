b8de37286ac1d94bcd1e426944df3466b634783c
Lesego <lesego.magosi@whitehatgaming.com>
2021-08-02 13:23:25 +0200

    hide values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   Api: {
     URL: 'https://api.openweathermap.org/data/2.5',
-    KEY: '85e9d9ad5ea0d7b7d428778307693cbf'
+    KEY: 'YOUR-API-KEY-GOES-HERE'
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   Api: {
     URL: 'https://api.openweathermap.org/data/2.5',
-    KEY: '85e9d9ad5ea0d7b7d428778307693cbf' //YOUR-API-KEY-GOES-HERE
+    KEY: 'YOUR-API-KEY-GOES-HERE'
   }
 };
 
