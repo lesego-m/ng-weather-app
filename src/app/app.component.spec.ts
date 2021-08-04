9efc23000717869e8acfb3dee3d57445b7d51f04
Lesego <lesego.magosi@whitehatgaming.com>
2021-08-04 02:58:28 +0200

    Add simple tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  const mainTitle: string = 'simple weather app';
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
@@ -16,16 +19,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-weather-app'`, () => {
+  it(`should have as title 'Simple Weather App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-weather-app');
+    expect(app.title).toEqual(mainTitle);
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-weather-app app is running!');
+    expect(compiled.querySelector('.ng-weather-title')?.textContent).toContain('Simple Weather App');
   });
 });
--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,25 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { WEATHER_MOCK_DATA } from 'src/app/models';
+import { SearchService } from 'src/app/services/search.service';
 
 import { SearchComponent } from './search.component';
 
-describe('SearchComponent', () => {
+xdescribe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let searchService: any;
 
   beforeEach(async () => {
+    searchService = jasmine.createSpyObj('SearchService', ['weatherSearch']);
     await TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      imports: [ReactiveFormsModule],
+      declarations: [ SearchComponent ],
+      providers: [
+        { provide: SearchService, useValue: searchService }
+      ]
     })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    .compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(SearchComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the search form', () => {
+    component.searchResults$ = of(WEATHER_MOCK_DATA);
+  });
+
+  it('should display search results', () => {
+    pending();
+  });
+
+  it('should trigger browser geolocation', () => {
+    pending();
   });
 });
