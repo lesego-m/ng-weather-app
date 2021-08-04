67e7efa2414656292ff6b409215ac7ccc6720a5e
Lesego <lesego.magosi@whitehatgaming.com>
2021-08-05 01:24:16 +0200

    Add app component tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
 
   const mainTitle: string = 'simple weather app';
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SearchStubComponent
       ],
-    }).compileComponents();
+    }).compileComponents()
+    .then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      app = fixture.componentInstance;
+    });
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'Simple Weather App'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app.title).toEqual(mainTitle);
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.ng-weather-title')?.textContent).toContain('Simple Weather App');
   });
+
+  it('should render app search components', () => {
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('app-search')).toBeTruthy();
+  });
 });
+
+@Component({
+  selector: 'app-search',
+  template: ''
+})
+class SearchStubComponent {
+}
